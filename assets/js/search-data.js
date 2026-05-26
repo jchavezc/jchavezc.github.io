9e62bf090f8ad40e5bbc1b8cce39362770dd4c56
jchavezc <53206788+jchavezc@users.noreply.github.com>
2026-05-26 18:48:19 +0000

    Deploying to gh-pages from @ jchavezc/jchavezc.github.io@17f4f27af5e419f04123af78c79bdefbaa226522 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -430,8 +430,8 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
+          section: "News",},{id: "projects-modeling-the-limit-order-book-from-a-microscopic-scale",
+          title: 'Modeling the Limit Order Book from a Microscopic scale',
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
